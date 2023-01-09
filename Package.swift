a4c9647853505306e96e40c78883c99bca83ceb5
joseantoniofacephi <89833605+joseantoniofacephi@users.noreply.github.com>
2023-01-09 16:40:35 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -16,14 +16,14 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/AgoraIO/AgoraRtcEngine_iOS", from: "3.4.3"),
         .package(url: "https://github.com/daltoniam/Starscream.git", from: "4.0.4"),
-       .package(name: "CorePackage", url: "https://github.com/facephi-clienters/SDK-CorePackage-SPM.git", from: "1.0.0"),
+       .package(name: "SDK-CorePackage-SPM", url: "https://github.com/facephi-clienters/SDK-CorePackage-SPM.git", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SDK-VideoIDPackage-SPM",
-            dependencies: ["videoid_component", "CorePackage", .product(
+            dependencies: ["videoid_component", "SDK-CorePackage-SPM", .product(
                 name: "RtcBasic",
                 package: "AgoraRtcEngine_iOS"), .product(
                     name: "Starscream",
