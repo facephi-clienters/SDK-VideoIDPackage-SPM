c11d4d2c4394992687b85e55e5e2efafc4b9ed6f
Facephi <developer@facephi.com>
2024-04-30 11:16:24 +0200

    New SPM Version: 2.0.2

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SDK-VideoIDPackage-SPM",
     defaultLocalization: "es",
-    platforms: [.iOS(.v12)],
+    platforms: [.iOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -16,27 +16,28 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "git@github.com:AgoraIO/AgoraRtcEngine_iOS", from: "3.4.3"),
-        .package(url: "git@github.com:daltoniam/Starscream.git", from: "4.0.6"),
-       .package(name: "SDK-CorePackage-SPM", url: "git@github.com:facephi-clienters/SDK-CorePackage-SPM.git", from: "1.5.8"),
-        .package(url: "git@github.com:airbnb/lottie-spm", from: "4.0.0")
+        .package(url: "https://github.com/AgoraIO/AgoraRtcEngine_iOS", .upToNextMinor(from: "4.3.1")),
+        .package(url: "https://github.com/daltoniam/Starscream.git", .upToNextMinor(from: "4.0.8")),
+        .package(url: "git@github.com:facephi-clienters/SDK-CorePackage-SPM.git", .upToNextMinor(from: "2.0.0")),
+        .package(url: "git@github.com:airbnb/lottie-spm", .exactItem("4.4.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SDK-VideoIDPackage-SPM",
-            dependencies: ["videoidComponent", "SDK-CorePackage-SPM",
-                .product(
-                    name: "RtcBasic",
-                    package: "AgoraRtcEngine_iOS"),
-                .product(
-                    name: "Starscream",
-                    package: "Starscream"),
-                .product(
-                    name: "Lottie",
-                    package: "lottie-spm"),
-            ]
+            dependencies: ["videoidComponent",
+                           "SDK-CorePackage-SPM",
+                           .product(
+                            name: "RtcBasic",
+                            package: "AgoraRtcEngine_iOS"),
+                           .product(
+                            name: "Starscream",
+                            package: "Starscream"),
+                           .product(
+                            name: "Lottie",
+                            package: "lottie-spm"),
+                          ]
         ),
         .binaryTarget(name: "videoidComponent", path: "videoidComponent.xcframework"),
     ]
